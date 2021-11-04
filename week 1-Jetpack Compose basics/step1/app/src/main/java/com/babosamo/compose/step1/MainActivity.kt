f4972ea091aa30732a80fcf0a19aa7249bb08355
babosamo <babosamo@gmail.com>
2021-11-04 17:10:46 +0900

    11. Animating your list

--- week 1-Jetpack Compose basics/step1/app/src/main/java/com/babosamo/compose/step1/MainActivity.kt
+++ week 1-Jetpack Compose basics/step1/app/src/main/java/com/babosamo/compose/step1/MainActivity.kt
@@ -3,11 +3,15 @@ package com.babosamo.compose.step1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -45,7 +49,7 @@ fun MyApp() {
 private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names){ name ->
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -54,7 +58,17 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+//    val extraPadding by animateDpAsState(
+//        if (expanded.value) 48.dp else 0.dp
+//    )
+
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -64,7 +78,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
