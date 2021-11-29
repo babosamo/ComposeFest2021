6bf59e8d484346c2b64d991867ca54e6b270222d
babosamo <babosamo@gmail.com>
2021-11-29 17:22:33 +0900

    6. Working with Text

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -17,7 +17,6 @@
 package com.codelab.theming.ui.start
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,13 +28,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
@@ -88,7 +87,7 @@ private fun AppBar() {
             Text(text = stringResource(R.string.app_title))
         },
         backgroundColor = MaterialTheme.colors.primarySurface
-        )
+    )
 }
 
 @Composable
@@ -103,6 +102,7 @@ fun Header(
     ) {
         Text(
             text = text,
+            style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
                 .semantics { heading() }
@@ -160,9 +160,14 @@ private fun PostMetadata(
         append(divider)
         append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
         append(divider)
+        val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+            background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+        )
         post.tags.forEachIndexed { index, tag ->
             if (index != 0) {
-                append(tagDivider)
+                withStyle(tagStyle) {
+                    append(tagDivider)
+                }
             }
             append(" ${tag.uppercase(Locale.getDefault())} ")
         }
