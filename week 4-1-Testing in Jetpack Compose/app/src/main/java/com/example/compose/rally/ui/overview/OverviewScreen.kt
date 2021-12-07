3f9adb7f6acdedd41e8cdfc0d10510733e116bd3
babosamo <babosamo@gmail.com>
2021-12-07 10:59:35 +0900

    6. Synchronization

--- week 4-1-Testing in Jetpack Compose/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
@@ -16,8 +16,7 @@
 
 package com.example.compose.rally.ui.overview
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -50,6 +49,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.compose.rally.R
 import com.example.compose.rally.RallyScreen
@@ -111,6 +111,17 @@ private fun AlertCard() {
             }
         }
     )
+
+//    val infiniteElevationAnimation = rememberInfiniteTransition()
+//    val animatedElevation2: Dp by infiniteElevationAnimation.animateValue(
+//        initialValue = 1.dp,
+//        targetValue = 8.dp,
+//        typeConverter = Dp.VectorConverter,
+//        animationSpec = infiniteRepeatable(
+//            animation = tween(500),
+//            repeatMode = RepeatMode.Reverse
+//        )
+//    )
     Card(elevation = animatedElevation.value) {
         Column {
             AlertHeader {
